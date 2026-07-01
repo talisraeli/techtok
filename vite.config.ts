be5df5967e55181d7d3c806d0af7451f77aa48b1
Tal Israeli <contact@talisrae.li>
2026-07-02 00:50:19 +0300

    fix: base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? '/techtok/' : '/',
+  base: '/',
   plugins: [
     react(),
     VitePWA({
